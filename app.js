6efa54b3c391f200d8ba2a75d8cd7dfd9301975a
Ron Elzuer D. Esmeralda <r.esmeralda@mec.local>
2020-03-03 15:33:44 +0800

    Second Commit

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.get("/about",function (req,res) {
     res.render("about");
 });
 
-app.listen(3000,function () {
+app.listen(process.env.PORT || 3000,function () {
     console.log("Server runs at port 3000");  
 });
